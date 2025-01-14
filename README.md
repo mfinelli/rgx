ad3f9307d1fc79b92a340e0a1adb828140f29cb1
Mario Finelli <mario@finel.li>
2025-01-14 20:01:01 +0100

    Run cargo generate ratatui/templates component
    Ignores the created LICENSE file because we'll be doing GPL.

---
+++ README.md
@@ -0,0 +1,5 @@
+# rgx
+
+[![CI](https://github.com/mfinelli/rgx/workflows/CI/badge.svg)](https://github.com/mfinelli/rgx/actions)
+
+command line regular expression tester
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use cli::Cli;
+use color_eyre::Result;
+
+use crate::app::App;
+
+mod action;
+mod app;
+mod cli;
+mod components;
+mod config;
+mod errors;
+mod logging;
+mod tui;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    crate::errors::init()?;
+    crate::logging::init()?;
+
+    let args = Cli::parse();
+    let mut app = App::new(args.tick_rate, args.frame_rate)?;
+    app.run().await?;
+    Ok(())
 }
